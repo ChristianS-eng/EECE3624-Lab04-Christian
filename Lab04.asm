f6dde157eaa7d147bf06f6cfa2e290e0a968195f
ChristianS-eng <cgsorensen1@mail.lipscomb.edu>
2026-09-21 20:24:17 -0500

    Updated files

--- Lab04.asm
+++ Lab04.asm
@@ -1,10 +1,10 @@
 /**************************************************************************
  *     File: Lab04.asm
- * Lab Name: 
- *   Author: 
- *  Created: 
+ * Lab Name: What's Your Calling?
+ *   Author: Christian Sorensen
+ *  Created: September 21, 2026
  *
- * This program...
+ * This program implements a recursive factorial function
  *************************************************************************/ 
  .def n = R16
 .def result = R17
@@ -20,7 +20,7 @@ main:       ; jump here on reset
 		ldi R16, low(RAMEND)
 		out SPL, R16
 
-		LDI  n, 4	; load a value into n
+		LDI  n, 5	; load a value into n
 		PUSH n	; push it on the stack
 		CALL factN	; calculate the factorial of n
 		POP  result	; pop result off stack
@@ -29,14 +29,49 @@ here:
 
 factN:
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	; Comments regarding the factN subroutine go here
+	; 
+	; factN is a recursive factorial function.
+	;
+	; It is a SIMPLE FUNCTION. It works on the values
+	; 1 through 5.
+	;
+	; An input of 0 WILL NOT OUTPUT 1
+	; AS MATHEMATICALLY EXPECTED, nor
+	; are inputs greater than 5 supported.
+	; 
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	; recursive factorial code begins here
 
+	IN YL, SPL	
+	IN YH, SPH	; Set pointer Y to match the stack pointer
 
+	LDD R18, Y+3 ; Fetch the value pushed
+
+	CPI R18, 0x01	; Test for base case
+	BRNE recursiveCase ; Recurse as necessary
+
+	RET	; Ready to pop to result
 
 recursiveCase:
 
+	DEC R18
+	PUSH R18	; Decrement "old argument" and push "new argument"
 
-	ret 
+	CALL factN ; Compute with new argument
+
+	IN YL, SPL
+	IN YH, SPH	; Set pointer Y to match the stack pointer
+
+	POP R19
+	LDD R18, Y+4 ; Fetch top two values of stack
+
+	MUL R18, R19 ; Multiply them
+
+	IN YL, SPL
+	IN YH, SPH ; Adjust Y
+
+	STD Y+3, R0	; Store product in address below
+				; Each n per function call is now n!
+
+	RET 
 	; return from the factN subroutine
